52d62888b9bf1f6ed57ed9bef66427ac0079093b
hami-dev <rinrin.xml@gmail.com>
2021-08-19 21:17:24 +0900

    feat : TODO 화면에 현재시간 표시 기능 추가

--- src/components/todo/template/head/TodoHead.tsx
+++ src/components/todo/template/head/TodoHead.tsx
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const TodoHead = () => {
     //@TODO 현재 시간을 표시해야합니다.
-    const dayString = "Tuesday";
-    const dateString = "July 20, 2021";
+    const [timeString, setTimeString] = useState("");
+
+    useEffect(() => {
+        setNowTime();
+    }, []);
+
+    const date = new Date();
+
+    const optionsForDate = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    const optionForDay = {
+        weekday: "long",
+    };
+
+    const dayString: string = date.toLocaleDateString("en-US", optionForDay);
+    const dateString: string = date.toLocaleDateString("en-US", optionsForDate);
+
+    function getNowTime() {
+        const date = new Date();
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+        const ampm = hour <= 12 ? "am" : "pm";
+
+        hour = hour % 12;
+        hour = hour ? hour : 12;
+        min = min < 10 ? "0" + min : min;
+        sec = sec < 10 ? "0" + sec : sec;
+
+        return `${ampm} ${hour} : ${min} : ${sec}`;
+    }
+
+    function setNowTime() {
+        const time = getNowTime();
+        setTimeString(time);
+
+        setInterval(() => {
+            const time = getNowTime();
+            setTimeString(time);
+        }, 1000);
+    }
 
     return (
         <TodoHeadBlock>
-            <DayText>{dayString}</DayText>
+            <Text>{dayString}</Text>
             <DateText>{dateString}</DateText>
+            <Text>{timeString}</Text>
         </TodoHeadBlock>
     );
 };
@@ -25,10 +69,10 @@ const TodoHeadBlock = styled.div`
 const DateText = styled.div`
     font-size: 26px;
     color: #119955;
-    padding-left: 10px;
+    padding: 0 10px;
 `;
 
-const DayText = styled.div`
+const Text = styled.div`
     font-size: 22px;
     color: #119955;
     padding-top: 5px;
